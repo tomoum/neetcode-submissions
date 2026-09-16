8c8438920c512ccd750264a9cbeba949ec5c7652
Muhab Tomoum <muhabtomoum@gmail.com>
2026-09-16 10:21:11 -0700

    Add: evaluate-reverse-polish-notation - submission-4

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-4.py
@@ -0,0 +1,57 @@
+"""
+Problem:
+1. Compute math result
+2. Pay attention to order of operations - not really just operate and store
+3. Truncate to 0 - int(a/b)
+4. No invalid operations
+
+Observations:
+1. All operators operate on 2 inputs
+2. no brackets
+
+e.g 
+5 + 3  -> 5 3 +
+
+Sol 
+
+1. iterate
+2. Check Curr in list of operators
+    - IF Operator: pop last 2 -> perform operation -> add result back to the stack
+    - ELSE: add to stack
+3. return top of stack
+
+"""
+import operator
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        operators = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": lambda a,b: int(operator.truediv(a,b)),
+        }
+
+        for token in tokens:
+            if token in operators:
+                a, b = int(stack.pop()), int(stack.pop())
+
+                if token == "/":
+                   a, b = b, a 
+                op_result = operators[token](a,b)
+                stack.append(op_result)
+            else:
+                stack.append(token)
+
+        return stack.pop()
+
+
+
+
+
+
+
+
+
+
